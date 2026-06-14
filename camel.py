80157382607462cf2c1e9bc46819d80d9810eb43
Dhruval-Rao-911 <dhruvalrao911@gmail.com>
2026-06-14 15:55:20 +0530

    Create camel.py

---
+++ camel.py
@@ -0,0 +1,12 @@
+word = input("camelCase: ")
+
+print("snake_case: ", end="")
+
+for char in word:
+    if char.isupper():
+        print("_" + char.lower(), end="")
+
+    else:
+        print(char, end="")
+
+print()
